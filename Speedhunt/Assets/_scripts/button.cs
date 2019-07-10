89bea1250499822a901ac54e0907c089df92f93c
mi5kas <el.miskauskas@gmail.com>
2019-07-10 17:55:57 +0300

    small

---
+++ Speedhunt/Assets/_scripts/button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class button : MonoBehaviour
+{
+    [SerializeField] Image template;
+    [SerializeField] Transform player;
+    [SerializeField] float opaqueDistance = 10;
+    Image iconClone;
+
+    void Start()
+    {
+        iconClone = Instantiate(template, Vector3.zero, Quaternion.identity).GetComponent<Image>();
+    }
+    void Update()
+    {
+        float distance = Vector3.Distance(this.transform.position, player.transform.position);
+        Vector3 visTest = Camera.main.WorldToViewportPoint(this.transform.position);
+        if ((visTest.x >= 0 && visTest.y >= 0) && (visTest.x <= 1 && visTest.y <= 1) && visTest.z >= 0)
+        {
+            Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+            iconClone.transform.position = namePose;
+            if (distance < 10f)
+            {
+                iconClone.color = new Color32(255, 255, 255, (byte)(255 - 255*(distance/opaqueDistance)));
+                //icon.rectTransform.sizeDelta = new Vector2(70 / distance, 70 / distance);
+                //icon.color = new Color32(255, 255, 255, 255);
+                //icon.gameObject.SetActive(true);
+            }
+            else
+                iconClone.color = new Color32(255, 255, 255, 0);
+        }
+        else
+            iconClone.color = new Color32(255, 255, 255, 0);
+    }
+    void OnMouseDown()
+    {
+
+    }
+}
