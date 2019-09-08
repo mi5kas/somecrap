6d26f67202462047e5f7dd63819957ce1085d556
Wattas <wattas@users.noreply.github.com>
2019-09-08 23:52:13 +0300

    tostandard

---
+++ Speedhunt/Assets/_scripts/setVariable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setVariable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string variableName;
+    [SerializeField] int variableValue;
+    void Start()
+    {
+        OnEnable();
+    }
+    void OnEnable()
+    {
+        PlayerPrefs.SetInt(variableName, variableValue);
+    }
+    // Update is called once per frame
+}
