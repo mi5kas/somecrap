d1c29979da6f613f8d187bbc8722d6a74c6532c8
Wattas <wattas@users.noreply.github.com>
2019-10-03 23:47:04 +0300

    gen

--- Speedhunt/Assets/_scripts/carGenerator.cs
+++ Speedhunt/Assets/_scripts/carGenerator.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class carGenerator : MonoBehaviour
 {
     [SerializeField] bool playerCar = true;
-    [SerializeField] Transform carObject;
-    [SerializeField] Transform[] suspension;
-    [SerializeField] Material carColor;
+    [SerializeField] int debugCar = 0;
     // Start is called before the first frame update
     void Start()
     {
         int tempCarID;
+        Transform[] suspension = new Transform[4];
+        PlayerPrefs.SetInt("currentCar", debugCar);
+        for(int i=0; i<4; i++)
+        {
+            suspension[i] = this.transform.GetChild(4+i);
+        }
         if(playerCar)
         {
-            tempCarID = PlayerPrefs.GetInt("carID");
-            carObject.GetChild(tempCarID).GetChild(PlayerPrefs.GetInt("carBody")).gameObject.SetActive(true);
-            suspension[0].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("carWheels")).gameObject.SetActive(true);
-            suspension[1].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("carWheels")).gameObject.SetActive(true);
-            suspension[2].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("carWheels")).gameObject.SetActive(true);
-            suspension[3].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("carWheels")).gameObject.SetActive(true);
+            tempCarID = PlayerPrefs.GetInt("currentCar");
+            this.transform.GetChild(0).GetChild(tempCarID).GetChild(PlayerPrefs.GetInt("car" + tempCarID + "Status")).gameObject.SetActive(true);
+            suspension[0].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("car" + tempCarID + "Wheels")).gameObject.SetActive(true);
+            suspension[1].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("car" + tempCarID + "Wheels")).gameObject.SetActive(true);
+            suspension[2].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("car" + tempCarID + "Wheels")).gameObject.SetActive(true);
+            suspension[3].GetChild(0).GetChild(0).GetChild(PlayerPrefs.GetInt("car" + tempCarID + "Wheels")).gameObject.SetActive(true);
         }
         else
         {
@@ -30,13 +34,13 @@ public class carGenerator : MonoBehaviour
             suspension[2].GetChild(0).GetChild(0).GetChild(randomWheels).gameObject.SetActive(true);
             suspension[3].GetChild(0).GetChild(0).GetChild(randomWheels).gameObject.SetActive(true);
         }
-        carObject.GetChild(tempCarID).gameObject.SetActive(true);
+        this.transform.GetChild(0).GetChild(tempCarID).gameObject.SetActive(true);
         if(tempCarID == 0) //Civic
         {
-            suspension[0].position = new Vector3(-0.85f, -0.20f, 1.323f);
-            suspension[1].position = new Vector3(0.85f, -0.20f, -1.48f);
-            suspension[2].position = new Vector3(0.85f, -0.2f, 1.323f);
-            suspension[3].position = new Vector3(-0.85f, -0.2f, -1.48f);
+            suspension[0].localPosition = new Vector3(-0.84f, -0.20f, 1.335f);
+            suspension[1].localPosition = new Vector3(0.84f, -0.20f, -1.475f);
+            suspension[2].localPosition = new Vector3(0.84f, -0.2f, 1.335f);
+            suspension[3].localPosition = new Vector3(-0.84f, -0.2f, -1.475f);
             suspension[0].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[1].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[2].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
@@ -44,10 +48,10 @@ public class carGenerator : MonoBehaviour
         }
         else if(tempCarID == 1) //E36
         {
-            suspension[0].position = new Vector3(-0.89f, -0.20f, 1.714f);
-            suspension[1].position = new Vector3(0.89f, -0.20f, -1.505f);
-            suspension[2].position = new Vector3(0.89f, -0.2f, 1.714f);
-            suspension[3].position = new Vector3(-0.89f, -0.2f, -1.505f);
+            suspension[0].localPosition = new Vector3(-0.9f, -0.20f, 1.73f);
+            suspension[1].localPosition = new Vector3(0.9f, -0.20f, -1.54f);
+            suspension[2].localPosition = new Vector3(0.9f, -0.2f, 1.73f);
+            suspension[3].localPosition = new Vector3(-0.9f, -0.2f, -1.54f);
             suspension[0].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
             suspension[1].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
             suspension[2].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
@@ -55,10 +59,10 @@ public class carGenerator : MonoBehaviour
         }
         else if(tempCarID == 2) //Golf
         {
-            suspension[0].position = new Vector3(-0.86f, -0.20f, 1.284f);
-            suspension[1].position = new Vector3(0.835f, -0.20f, -1.486f);
-            suspension[2].position = new Vector3(0.835f, -0.2f, 1.284f);
-            suspension[3].position = new Vector3(-0.86f, -0.2f, -1.486f);
+            suspension[0].localPosition = new Vector3(-0.87f, -0.20f, 1.29f);
+            suspension[1].localPosition = new Vector3(0.87f, -0.20f, -1.5f);
+            suspension[2].localPosition = new Vector3(0.87f, -0.2f, 1.29f);
+            suspension[3].localPosition = new Vector3(-0.87f, -0.2f, -1.5f);
             suspension[0].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[1].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[2].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
@@ -66,16 +70,27 @@ public class carGenerator : MonoBehaviour
         }
         else if(tempCarID == 3) //Miata
         {
-            suspension[0].position = new Vector3(-0.82f, -0.20f, 1.28f);
-            suspension[1].position = new Vector3(0.82f, -0.20f, -1.24f);
-            suspension[2].position = new Vector3(0.82f, -0.2f, -1.24f);
-            suspension[3].position = new Vector3(-0.82f, -0.2f, -1.486f);
+            suspension[0].localPosition = new Vector3(-0.81f, -0.20f, 1.27f);
+            suspension[1].localPosition = new Vector3(0.81f, -0.20f, -1.21f);
+            suspension[2].localPosition = new Vector3(0.81f, -0.2f, 1.27f);
+            suspension[3].localPosition = new Vector3(-0.81f, -0.2f, -1.21f);
             suspension[0].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[1].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[2].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
             suspension[3].GetChild(0).GetChild(0).localScale = new Vector3(0.9f, 0.9f, 0.9f);
         }
-        else if(tempCarID == 4) //Skyline
+        else if(tempCarID == 4) //RX8
+        {  
+            suspension[0].localPosition = new Vector3(-0.93f, -0.20f, 1.63f);
+            suspension[1].localPosition = new Vector3(0.93f, -0.20f, -1.475f);
+            suspension[2].localPosition = new Vector3(0.93f, -0.2f, 1.63f);
+            suspension[3].localPosition = new Vector3(-0.93f, -0.2f, -1.475f);
+            suspension[0].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
+            suspension[1].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
+            suspension[2].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
+            suspension[3].GetChild(0).GetChild(0).localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(tempCarID == 5) //Skyline
         {
             suspension[0].position = new Vector3(-0.85f, -0.20f, 1.42f);
             suspension[1].position = new Vector3(0.85f, -0.20f, -1.48f);
