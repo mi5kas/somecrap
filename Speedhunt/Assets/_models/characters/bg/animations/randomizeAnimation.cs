dbf0d6b486ef8649ad1678bc7e6c1ff96a3af84b
Wattas <wattas@users.noreply.github.com>
2019-10-07 23:36:05 +0300

    bg

---
+++ Speedhunt/Assets/_models/characters/bg/animations/randomizeAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomizeAnimation : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        InvokeRepeating("ChangeAnim", 1f, 1f);
+    }
+    void ChangeAnim()
+    {
+        animator.SetInteger("whichAnim", Random.Range(0, 7));
+    }
+    // Update is called once per fram
+}
