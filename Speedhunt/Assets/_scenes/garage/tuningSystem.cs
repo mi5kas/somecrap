074f9151fbed550a41d07fa9f794e75d11a244a9
mi5kas <el.miskauskas@gmail.com>
2020-01-07 16:45:24 +0200

    smol

--- Speedhunt/Assets/_scenes/garage/tuningSystem.cs
+++ Speedhunt/Assets/_scenes/garage/tuningSystem.cs
@@ -11,7 +11,7 @@ public class tuningSystem : MonoBehaviour
     [SerializeField] GameObject[] tuningCameras;
     [SerializeField] Text moneyText;
     [SerializeField] Material playerMaterial;
-    [SerializeField] GameObject tuningButtons;
+    [SerializeField] Button[] tuningButtons;
     [SerializeField] Text[] levelTexts;
     [SerializeField] Text[] costTexts;
     Color defaultColor;
@@ -47,7 +47,7 @@ public class tuningSystem : MonoBehaviour
             colorPalette.gameObject.SetActive(true);
             colorPalette.CurrentColor = playerMaterial.GetColor("_Color");
             tuningCameras[0].SetActive(true);
-            tuningButtons.SetActive(false);
+            tuningButtons[0].transform.parent.gameObject.SetActive(false);
         } 
     }
     public void ShowColor()
@@ -56,17 +56,74 @@ public class tuningSystem : MonoBehaviour
     }
     public void RejectTuning()
     {
+        if(whichModConfirmed < 3)
+        {
+            confirmMod.gameObject.SetActive(false);
+        }
         else if(whichModConfirmed == 4)
         {
             colorPalette.gameObject.SetActive(false);
             playerMaterial.SetColor("_Color", defaultColor);
             tuningCameras[0].SetActive(false);
-            tuningButtons.SetActive(true);
+            tuningButtons[0].transform.parent.gameObject.SetActive(true);
         }
     }
     void UpdateButtons()
     {
-
+        int currentMoney = PlayerPrefs.GetInt("money");
+        if(currentMoney < 300*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine")))
+        {
+            tuningButtons[0].interactable = false;
+            levelTexts[0].transform.parent.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            levelTexts[0].color =  new Color(1, 1, 1, 0.5f);
+            costTexts[0].color =  new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 200*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Suspension")))
+        {
+            tuningButtons[1].interactable = false;
+            levelTexts[1].transform.parent.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            levelTexts[1].color =  new Color(1, 1, 1, 0.5f);
+            costTexts[1].color =  new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 400*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Nitrous")))
+        {
+            tuningButtons[2].interactable = false;
+            levelTexts[2].transform.parent.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            levelTexts[2].color =  new Color(1, 1, 1, 0.5f);
+            costTexts[2].color =  new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 500 || PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Status") != 0)
+        {
+            tuningButtons[3].interactable = false;
+            tuningButtons[3].GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 1000 || PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Status") == 0)
+        {
+            tuningButtons[4].interactable = false;
+            tuningButtons[4].GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 1000 || PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Status") == 2)
+        {
+            tuningButtons[5].interactable = false;
+            tuningButtons[5].GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 400)
+        {
+            tuningButtons[6].interactable = false;
+            tuningButtons[6].GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        if(currentMoney < 200 || PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Wheels") == 0)
+        {
+            tuningButtons[7].interactable = false;
+            tuningButtons[7].GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        moneyText.text = "MONEY: " + currentMoney + "$";
+        levelTexts[0].text = "LEVEL " + PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine");
+        costTexts[0].text = 400*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine")) + "$";
+        levelTexts[1].text = "LEVEL " + PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Suspension");
+        costTexts[1].text = 200*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Suspension")) + "$";
+        levelTexts[2].text = "LEVEL " + PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Nitrous");
+        costTexts[1].text = 400*(1+PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Nitrous")) + "$";
     }
     public void BuyTuning()
     {
@@ -75,19 +132,16 @@ public class tuningSystem : MonoBehaviour
             confirmMod.transform.parent.gameObject.SetActive(false);
             PlayerPrefs.SetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine", PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine")+1);
             PlayerPrefs.SetInt("money", PlayerPrefs.GetInt("money")-300*PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine"));
-            UpdateButtons();
-            //levelTexts[0].text = "LEVEL " + PlayerPrefs.GetInt("car" + PlayerPrefs.GetInt("currentCar") + "Engine");
         }
         else if(whichModConfirmed == 4)
         {
             colorPalette.gameObject.SetActive(false);
             defaultColor = colorPalette.CurrentColor;
             tuningCameras[0].SetActive(false);
-            tuningButtons.SetActive(true);
+            tuningButtons[0].transform.parent.gameObject.SetActive(true);
             PlayerPrefs.SetInt("money", PlayerPrefs.GetInt("money")-1000);
-            moneyText.text = "MONEY: " + PlayerPrefs.GetInt("money") + "$";
-            UpdateButtons();
         }
+        UpdateButtons();
     }
     // Update is called once per frame
     void Update()
