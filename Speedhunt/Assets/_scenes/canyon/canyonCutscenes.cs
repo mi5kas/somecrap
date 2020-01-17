b2d5066e199f2ad6090559da856f2c124eef6abd
mi5kas <el.miskauskas@gmail.com>
2020-01-17 16:48:35 +0200

    ??

---
+++ Speedhunt/Assets/_scenes/canyon/canyonCutscenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canyonCutscenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Animator enemyCar;
+    [SerializeField] Animator playerCar;
+    public void PlayAnimation(int which)
+    {
+        if(which == 0)
+        {
+            playerCar.Play("player1");
+        }
+        else if(which == 1)
+        {
+            playerCar.Play("player2");
+            enemyCar.Play("enemy2");
+        }
+        else if(which == 2)
+        {
+            playerCar.Play("player3");
+            enemyCar.Play("enemy3");
+        }
+    }
+    // Update is called once per frame
+}
