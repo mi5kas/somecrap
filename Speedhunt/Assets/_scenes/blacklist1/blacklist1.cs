332b380de8956e0b31e370f2ea38af6d201fc6cd
mi5kas <33864467+mi5kas@users.noreply.github.com>
2020-03-03 08:38:31 +0200

    blacklist

---
+++ Speedhunt/Assets/_scenes/blacklist1/blacklist1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blacklist1 : MonoBehaviour
+{
+    [SerializeField] Transform scottyNeck;
+    [SerializeField] Transform[] eyes;
+    [SerializeField] Transform neckLookAt;
+    [SerializeField] Transform eyesLookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        scottyNeck.LookAt(neckLookAt);
+        eyes[0].LookAt(eyesLookAt);
+        eyes[1].LookAt(eyesLookAt);
+    }
+}
